d31b2fb1a844486517cdc9a86ee37a8fbe264aee
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-10-24 17:31:10 +0300

    eco-cosmetics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#shop') {
+    document.body.classList.add('page__none--with-menu');
+  } else {
+    document.body.classList.remove('page__none--with-menu');
+  }
+});
